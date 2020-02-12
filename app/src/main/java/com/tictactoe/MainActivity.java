abe4310bda42bf0f53a575d29ecbbb54fefb2d4c
Prithvi <prithvikhanna10@gmail.com>
2020-02-12 23:59:45 +0530

    first commit

---
+++ app/src/main/java/com/tictactoe/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
